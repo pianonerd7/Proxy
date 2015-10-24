9d081a432dfa3c52e2a9190b92c6083cefd90ea2
Anna <pianonerd7@gmail.com>
2015-10-24 14:32:26 -0400

    Accepts new requests

--- src/RequestListener.java
+++ src/RequestListener.java
@@ -1,4 +1,19 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class RequestListener {
-
+	
+	private static final int portNum = 5026;
+	
+	public static void main(String[] args) throws IOException {
+		
+		ServerSocket welcomeSocket = new ServerSocket(portNum);
+		
+		while (true) {
+			Socket connectionSocket = welcomeSocket.accept();
+			
+			//BufferedReader 
+		}
+	}
 }

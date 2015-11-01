7efb88536d7e30a61a3377923ade07d8d1ee9d73
Anna <pianonerd7@gmail.com>
2015-11-01 00:21:19 -0400

    i think it's working

--- src/RequestSender.java
+++ src/RequestSender.java
@@ -21,33 +21,59 @@ public class RequestSender implements Runnable {
 		
 		try {
 			InputStream requestInput = requestSocket.getInputStream();
-			ByteBuffer bufferInput = ByteBuffer.allocate(16384);
+			ByteBuffer bufferInput = ByteBuffer.allocate(16*1024);
 
 			int temp = requestInput.read();
 			System.out.println("LALALALALA");
 			while (temp != -1) {
-				System.out.print((char)temp);
 				bufferInput.put((byte)temp);
 				
 				if (isEndReq((char)temp)) {
+					RequestEditor editor = new RequestEditor(bufferInput, requestIP.getHostAddress());
+					bufferInput.clear();
+					System.out.print(editor);
+					System.out.flush();
+					Socket outSocket = new Socket(editor.getHostName(), 80);
+					RequestProcessor processor = new RequestProcessor(requestSocket, outSocket);
+					System.out.println("here1");
+					new Thread(processor).start();
+					
+					OutputStream outputStream = outSocket.getOutputStream();
+					outputStream.write(editor.toByteArr());
 					
 				}
 				temp = requestInput.read();
 				
-				Socket outSocket = new Socket("case.edu", 80);
-				OutputStream output = outSocket.getOutputStream();
-				RequestProcessor processor = new RequestProcessor(requestSocket, outSocket);
-				new Thread(processor).start();
 			}
+			requestSocket.close();
 		} 
 		catch (IOException e) {
 			System.out.println(e.getMessage());
 		}
 	}
 	
-	private boolean isEndReq(char curChar) {
+	int endCount = 0;
+	private boolean isEndReq(char currChar) {
 		
-		return true;
+		if (endCount == 0 && currChar == '\r') {
+			endCount++;
+			System.out.println(endCount);
+		}
+		else if (endCount == 1 && currChar == '\n') {
+			endCount++;
+			System.out.println(endCount);
+		}
+		else if (endCount == 2 && currChar == '\r') {
+			endCount++;
+			System.out.println(endCount);
+		}
+		else if (endCount == 3 && currChar == '\n') {
+			return true;
+		}
+		else {
+			endCount = 0;
+		}
+		return false;
 	}
 
 }
